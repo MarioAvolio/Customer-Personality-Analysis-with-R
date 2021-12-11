278d8f40e372d71f829dd43380f37fca6864f9cc
MarioAvolio <marioavolio1999@gmail.com>
2021-12-11 17:08:13 +0100

    Read DataSet

--- analysis.R
+++ analysis.R
@@ -1,2 +1,8 @@
-dataSet <- read.csv("marketing_campaign.csv")
-print(dataSet)
+# --------------------------------------------------- read dataset
+dataSet <- read.csv("marketing_campaign.csv", header=TRUE, sep="\t") # use TAB as separator!
+head(dataSet) # first 6 examples
+col(dataSet)
+row(dataSet)
+# --------------------------------------------------- PRINCIPAL COMPONENT ANALISYS - PCA
+
+
