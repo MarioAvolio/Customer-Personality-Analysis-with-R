c61073bb01a688ba36f69066fd0a6bf360b2eced
MarioAvolio <marioavolio1999@gmail.com>
2021-12-11 22:06:34 +0100

    Implement missind data resolve

--- analysis.R
+++ analysis.R
@@ -1,8 +1,33 @@
-# --------------------------------------------------- read dataset
+# --------------------------------------------------- DATA PROCESSING
+
+# Read DataSet
 dataSet <- read.csv("marketing_campaign.csv", header=TRUE, sep="\t") # use TAB as separator!
 head(dataSet) # first 6 examples
 col(dataSet)
 row(dataSet)
+View(dataSet)
+
+# Missing Data
+# use average for missing data
+
+
+for(i in 1:ncol(dataSet)) {       # for-loop over columns
+  
+  print(paste(c("coloumn n.", i), collapse = " "))
+  dataSet[ , i] <- ifelse(is.na(dataSet[ ,i]), # is.na check is a value is not available
+                          ave(dataSet[, i], FUN = function(x) mean(x, na.rm = TRUE)), # if is not available change with average
+                          dataSet[ ,i] # else
+                          ) 
+  
+}
+
+
+
+
+# split in Training-Set and Test-Set
+install.packages('caTools')
+library(caTools)
+
 # --------------------------------------------------- PRINCIPAL COMPONENT ANALISYS - PCA
 
 
