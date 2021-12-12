96eeeca0f7bd60c6c4f91fe363669c91ce818828
MarioAvolio <marioavolio1999@gmail.com>
2021-12-12 11:33:09 +0100

    Split data into TrainingSet and TestSet

--- Model/DataPreprocessing.R
+++ Model/DataPreprocessing.R
@@ -1,7 +1,18 @@
-# --------------------------------------------------- DATA PROCESSING
+# --------------------------------------------------- DATA PREPROCESSING
+# 1 Read DataSet
+# 2 Convert categorical data to factor data
+# 3 Fix Missing Data with average data
+# 4 Split DataSet into Training-Set and Test-Set
+# 5 Feature Scaling
+# ---------------------------------------------------
+
+
+
 
 # Read DataSet
 dataSet <- read.csv("marketing_campaign.csv", header=TRUE, sep="\t",  stringsAsFactors=T) # use TAB as separator!
+
+
 
 # Convert categorical to factor
 converterCharacterToFactor <- function(column) {
@@ -13,18 +24,15 @@ converterCharacterToFactor <- function(column) {
                    labels = 1:length(uniqueValue)) # Convert character column to factor 
   
 }
-
 dataSet$Education <- converterCharacterToFactor(dataSet$Education)
 dataSet$Marital_Status <- converterCharacterToFactor(dataSet$Marital_Status)
 dataSet$Dt_Customer <- converterCharacterToFactor(dataSet$Dt_Customer)
 
 
+
 # Missing Data
 # use average for missing data
-
 for(i in 1:ncol(dataSet)) {       # for-loop over columns
-  
-  print(paste(c("coloumn n.", i), collapse = " "))
   dataSet[ , i] <- ifelse(is.na(dataSet[ ,i]), # is.na check is a value is not available
                           ave(dataSet[, i], FUN = function(x) mean(x, na.rm = TRUE)), # if is not available change with average
                           dataSet[ ,i] # else
@@ -34,23 +42,15 @@ for(i in 1:ncol(dataSet)) {       # for-loop over columns
 
 
 
-
-# split in Training-Set and Test-Set
+# Split in Training-Set and Test-Set
 install.packages('caTools')
 library(caTools)
+set.seed(17538)
+split <- sample.split(dataSet$ID, SplitRatio = 0.8)
+trainingSet <- subset(dataSet, split == TRUE)
+testSet <- subset(dataSet, split == FALSE)
 
 
 
-
-
-
-
-
-
-
-
-
-
-# --------------------------------------------------- PRINCIPAL COMPONENT ANALISYS - PCA
-
+# Feature Scaling
 
