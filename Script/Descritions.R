6373aae9ef2d41b79c5eaedce2fefd5ee22f86ec
MarioAvolio <marioavolio1999@gmail.com>
2021-12-20 15:39:39 +0100

    Split EDA

---
+++ Script/Descritions.R
@@ -0,0 +1,77 @@
+library(dplyr) # glimpse
+library(ggplot2)
+library(naniar)
+library(tidyr)
+library(lubridate)
+library(ggcorrplot)
+library(caTools)
+
+########################################################################
+#                                                                      #
+#                           Read Dataset                               #
+#                                                                      #
+######################################################################## 
+customers <- read.csv(paste(getwd(),"/Data/marketing_campaign.csv",sep = ""), header=TRUE, sep="\t",  stringsAsFactors=F) # use TAB as separator!
+########################################################################
+
+
+
+
+
+
+########################################################################
+#                                                                      #
+#                               SUMMARY                                #
+#                                                                      #
+########################################################################
+summary(customers) # most important!
+dim(customers)
+head(customers, n=6)
+glimpse(customers)
+sapply(dataSet, class)
+# NOTE:
+# - There are some categorical features, so we will need to encode into numeric form as we proceed.
+########################################################################
+
+
+
+
+
+########################################################################
+#                                                                      #
+#                             MISSING VALUE                            #
+#                                                                      #
+########################################################################
+hist(customers$Income,40,col="#adcae6")
+ggplot(customers, aes(y = Income)) + geom_boxplot()
+n_miss(customers) # counting the total number of missing values in the data
+miss_var_summary(customers) # Summarizing missingness in each variable 
+
+
+# NOTE:
+#   
+# - There are 24 missing values in the income variable.
+# - Also, we can see that the maximum value of the income variable is larger than the 3rd quantile.
+########################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Script/EDA.R
+++ Script/EDA.R
@@ -1,48 +1,3 @@
-library(dplyr) # glimpse
-library(ggplot2)
-library(naniar)
-library(tidyr)
-library(lubridate)
-library(ggcorrplot)
-library(caTools)
-
-########################################################################
-#                                                                      #
-#                           Read Dataset                               #
-#                                                                      #
-######################################################################## 
-customers <- read.csv(paste(getwd(),"/Data/marketing_campaign.csv",sep = ""), header=TRUE, sep="\t",  stringsAsFactors=F) # use TAB as separator!
-########################################################################
-
-
-
-
-
-
-########################################################################
-#                                                                      #
-#                               SUMMARY                                #
-#                                                                      #
-########################################################################
-summary(customers) # most important!
-dim(customers)
-head(customers, n=6)
-glimpse(customers)
-sapply(dataSet, class)
-# NOTE:
-# - There are some categorical features, so we will need to encode into numeric form as we proceed.
-########################################################################
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -145,52 +100,6 @@ ggplot(customers, aes(Year_Birth)) + geom_boxplot()  #aes => Aesthetics layer; g
 # - Dt_Customer that indicates a cutomer joined the database is not parsed as Date object.
 # - We also noted from looking at the summary statistics, the minimum year of birth 1893. This became clear when we plotted a boxplot.
 ########################################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-########################################################################
-#                                                                      #
-#                             MISSING VALUE                            #
-#                                                                      #
-########################################################################
-hist(customers$Income,40,col="#adcae6")
-ggplot(customers, aes(y = Income)) + geom_boxplot()
-n_miss(customers) # counting the total number of missing values in the data
-miss_var_summary(customers) # Summarizing missingness in each variable 
-
-
-# NOTE:
-#   
-# - There are 24 missing values in the income variable.
-# - Also, we can see that the maximum value of the income variable is larger than the 3rd quantile.
-########################################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
