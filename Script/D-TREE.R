859d6d506872d400edabbb266216b5bdbce3ecd2
Rocco Gianni Rapisarda <r.rapisarda2@campus.unimib.it>
2022-01-10 16:33:19 +0100

    update d-tree

--- Script/D-TREE.R
+++ Script/D-TREE.R
@@ -1,6 +1,5 @@
 # execute DataPreprocessing
 source(paste(getwd(),"/Script/PCA.R",sep = "")) 
-
 ########################################################################
 #                                                                      #
 #                               LIBRARY                                #
@@ -9,103 +8,68 @@ source(paste(getwd(),"/Script/PCA.R",sep = ""))
 library("rpart")
 library("rpart.plot")
 library("tidyverse")
-
 library("RColorBrewer")
-
-
 ########################################################################
 
 
-#decision tree considering all variables
-decisionTreeAll = rpart(trainingSet_input$Response ~ ., data=trainingSet_input, method="class")
-rpart.plot(decisionTreeAll)
 
-#We can now use our decision tree model, already trained, to make a prediction on our test set
-trainingSet_input$Prediction1 <- predict(decisionTreeAll, trainingSet_input, type = "class")
+# nel nuovo dataset preso da pca aggiungo la colonna response
+trainingSet_input$Response<-trainingSet$Response
 
-confusion.matrix1 = table(trainingSet_input$Response, trainingSet_input$Prediction1)
+# costruisco l'albero
+response.default.tree <- rpart(Response ~ ., 
+                               data = trainingSet_input, 
+                           method = "class")
+?rpart
 
-confusion.matrix1
 
-#     0    1
-# 0 1486   39
-# 1  147  120
 
-sum(diag(confusion.matrix1))/sum(confusion.matrix1) 
-#equal to 0.8962, it classifies very well
+?prp
 
-#summary of the decision tree
-summary(decisionTreeAll)
+prp( response.default.tree, 
+     type = 1, extra = 1, varlen = -10, 
+     box.col = ifelse(response.default.tree$frame$var == "<leaf>", 'gray', 'white'))
 
-#List complexity parameter
-printcp(decisionTreeAll)
 
-#        CP    nsplit rel error  xerror     xstd
-#1 0.067416      0   1.00000    1.00000   0.056456
-#2 0.022472      3   0.77903    0.78652   0.050996
-#3 0.014981      5   0.73408    0.79775   0.051310
-#4 0.011236      6   0.71910    0.81273   0.051724
-#5 0.010000      8   0.69663    0.83521   0.052334
-
-#So the cp of the root is 0.067 + 0.022 + 0.015 + 0.011 + 0.010 = 0,125
-
-#plot the decision tree complexity parameter
-plotcp(decisionTreeAll)
-
-#NOTE: n short, the greater the CP value, the fewer the number of splits there are.
-#     CP is the amount by which splitting that node improved the relative error.
+response.default.tree.pred <- predict(response.default.tree, trainingSet_input, type = "class")
+confusionMatrix.default<-confusionMatrix(response.default.tree.pred, as.factor(trainingSet_input$Response), positive = "1")
+confusionMatrix.default
 
 
 
 
-prunedDecisionTreeAll = prune(decisionTreeAll, cp= 0.05)
-rpart.plot(prunedDecisionTreeAll)
+cv.ct <- rpart( Response ~ ., 
+                data = trainingSet_input, 
+                method = "class", 
+                cp = 0, minsplit = 2, 
+                xval = 10)
+printcp(cv.ct)
 
 
+# min error
+minerror <- min(cv.ct$cptable[ , 4])
+minerror
 
-####################################################################################
+# corresponding xstd
+minerrorstd <- cv.ct$cptable[cv.ct$cptable[,4] == minerror, 5]
+minerrorstd
 
-#lets see what are the changes considering the first four variables
-#decision tree using the first 4 variables
-decisionTreeFew = rpart(Response ~ Education + Marital_Status + Income + Total_Childs , data=trainingSet_input, method="class")
+# list of trees where xerror is less than minerror + minerrorstd
+simplertrees <- cv.ct$cptable[cv.ct$cptable[,4] < minerror + minerrorstd, ]
+simplertrees
 
-rpart.plot(decisionTreeFew)
+# cp of the simplest of those trees
+bestcp <- simplertrees[1, 1]
+bestcp
 
+response.best.tree <- prune( cv.ct, 
+                             cp = bestcp )
 
-
-#We can now use our decision tree model, already trained, to make a prediction on our test set
-trainingSet_input$Prediction2 <- predict(decisionTreeFew, trainingSet_input, type = "class")
-
-confusion.matrix2 = table(trainingSet_input$Response, trainingSet_input$Prediction2)
-
-confusion.matrix2
-
-sum(diag(confusion.matrix2))/sum(confusion.matrix2) #equal to 0.861
-
-#summary of the decision tree
-summary(decisionTreeFew)
-
-#List complexity parameter
-printcp(decisionTreeFew)
- 
-#plot the decision tree complexity parameter
-plotcp(decisionTreeFew)
-
-#NOTE: n short, the greater the CP value, the fewer the number of splits there are.
-#     CP is the amount by which splitting that node improved the relative error.
-
-#        CP nsplit rel error  xerror     xstd
-#1 0.035581      0   1.00000 1.00000 0.056456
-#2 0.010000      2   0.92884 0.94757 0.055208
-
-#So the cp of the root is 0.03+0.01 = 0.04
+prp( response.best.tree, 
+     type = 1, extra = 1, varlen = -15, cex = 0.5,
+     box.col = ifelse(response.best.tree$frame$var == "<leaf>", 'gray', 'white' ))
 
 
-prunedDecisionTreeFew = prune(decisionTreeFew, cp= 0.05)
-rpart.plot(prunedDecisionTreeFew)
-
-#with cp equal to 0.05 there is only one node comparing the original decision tree
-
-#the % of decsionTreeFew is less than decisionTreeAll (3% less).The number of 
-# considered variables is lower. 
+response.best.tree.pred <- predict(response.best.tree, trainingSet_input, type = "class")
+confusionMatrix2<-confusionMatrix(response.best.tree, as.factor(trainingSet_input$Response), positive = "1")
 
